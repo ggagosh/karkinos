76f0a12eb1d7489443c700bce09d9a624102acc9
Giorgi Gagoshidze <g.gagoshidze@omedia.ge>
2022-09-15 12:01:43 +0400

    add bins

--- src/gen.rs
+++ src/gen.rs
@@ -4,11 +4,11 @@ extern crate serde_json;
 use schemars::schema_for;
 use std::fs::File;
 
-#[path = "../types.rs"]
+#[path = "types.rs"]
 mod types;
 
 fn main() {
-    let dest = File::create("krk-schema.json").expect("can't create json schema file :(");
+    let dest = File::create("../krk-schema.json").expect("can't create json schema file :(");
     let schema = schema_for!(types::ScrapeRoot);
 
     serde_json::to_writer_pretty(dest, &schema).unwrap();
